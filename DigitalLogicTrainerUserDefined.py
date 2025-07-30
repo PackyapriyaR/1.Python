57c00cf5f3d0d370597537b184f93a6f5d1d69d1
Packyapriya R <rampree@gmail.com>
2025-07-30 22:56:16 +0530

    Add files via upload

---
+++ DigitalLogicTrainerUserDefined.py
@@ -0,0 +1,18 @@
+class TruthTable():
+    def ANDTable():
+        print(0,"|",0,"|",0)
+        print(0,"|",1,"|",0)
+        print(1,"|",0,"|",0)
+        print(1,"|",1,"|",1)
+    def ORTable():
+        print(0,"|",0,"|",0)
+        print(0,"|",1,"|",1)
+        print(1,"|",0,"|",1)
+        print(1,"|",1,"|",1) 
+    def XORTable():
+        print(0,"|",0,"|",0)
+        print(0,"|",1,"|",1)
+        print(1,"|",0,"|",1)
+        print(1,"|",1,"|",0)        
+    def NOTTable():
+        print("Truth Table not applicable")
